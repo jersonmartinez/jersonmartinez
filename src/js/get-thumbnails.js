7889a0c4a5f391bcdb91f3d20d486f119d30569c
Jerson Martínez <hotaka01@grupomrw.onmicrosoft.com>
2024-02-09 01:51:20 -0600

    GitHub Actions

--- src/js/get-thumbnails.js
+++ src/js/get-thumbnails.js
@@ -8,12 +8,12 @@ axios.get('https://www.youtube.com/feeds/videos.xml?channel_id=UCHQb90WIYhLUObEc
     .then(response => {
         parser.parseStringPromise(response.data).then(result => {
             const entries = result.feed.entry.slice(0, MAX_RESULTS);
-            let videos = '<table style="width: 100%; border-collapse: collapse;"><tr>';
+            let videos = '<table style="width: 100%; border-collapse: collapse; overflow-y: hidden;"><tr>';
             entries.forEach((entry, index) => {
                 const videoId = entry['yt:videoId'][0];
                 const title = entry.title[0];
                 const thumbnail = `https://img.youtube.com/vi/${videoId}/0.jpg`;
-                videos += `<td style="border: 1px solid #ddd; padding: 8px; text-align: center;"><a href="https://www.youtube.com/watch?v=${videoId}" target="_blank"><img src="${thumbnail}" alt="${title}" style="width:210px; border-radius: 5px;"><br>${title}</a></td>`;
+                videos += `<td style="padding: 8px; text-align: center;"><a href="https://www.youtube.com/watch?v=${videoId}" target="_blank"><img src="${thumbnail}" alt="${title}" style="width:210px; border-radius: 5px;"><br>${title}</a></td>`;
                 if ((index + 1) % 3 === 0) {
                     videos += '</tr><tr>';
                 }
