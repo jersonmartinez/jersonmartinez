c5bcb10ad458c8a5464daa431c988c40449551bc
Jerson Martínez <hotaka01@grupomrw.onmicrosoft.com>
2024-02-09 09:00:18 -0600

    Refactor video thumbnail display in get-thumbnails.js

--- src/js/get-thumbnails.js
+++ src/js/get-thumbnails.js
@@ -2,18 +2,18 @@ const axios = require('axios');
 const xml2js = require('xml2js');
 
 const parser = new xml2js.Parser();
-const MAX_RESULTS = 6; // Limit to 6 videos
+const MAX_RESULTS = 6;
 
 axios.get('https://www.youtube.com/feeds/videos.xml?channel_id=UCHQb90WIYhLUObEc8uVJR6A')
     .then(response => {
         parser.parseStringPromise(response.data).then(result => {
             const entries = result.feed.entry.slice(0, MAX_RESULTS);
-            let videos = '<table style="width: 100%; border-collapse: collapse; overflow-y: hidden; border: none;"><tr>';
+            let videos = '<table style="width: 100%; overflow-y: hidden; overflow-x: hidden; border: none;"><tr>';
             entries.forEach((entry, index) => {
                 const videoId = entry['yt:videoId'][0];
                 const title = entry.title[0];
                 const thumbnail = `https://img.youtube.com/vi/${videoId}/0.jpg`;
-                videos += `<td style="padding: 0px; margin:0px; text-align: center;"><a href="https://www.youtube.com/watch?v=${videoId}" target="_blank"><img src="${thumbnail}" alt="${title}" style="width:100%; border-radius: 5px;"><br>${title}</a></td>`;
+                videos += `<td style="padding: 0px; margin:0px; text-align: center;"><a href="https://www.youtube.com/watch?v=${videoId}" target="_blank"><img src="${thumbnail}" alt="${title}" width="480" height="360"><br>${title}</a></td>`;
                 if ((index + 1) % 3 === 0) {
                     videos += '</tr><tr>';
                 }
