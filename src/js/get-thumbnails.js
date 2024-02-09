92592ad4aa33d0ee310a7f14ec42dc008cd538ea
Jerson Martínez <hotaka01@grupomrw.onmicrosoft.com>
2024-02-09 00:56:27 -0600

    Adding GitHub Actions

--- src/js/get-thumbnails.js
+++ src/js/get-thumbnails.js
@@ -3,17 +3,22 @@ const xml2js = require('xml2js');
 
 const parser = new xml2js.Parser();
 
-axios.get('https://www.youtube.com/feeds/videos.xml?channel_id=UCHQb90WIYhLUObEc8uVJR6A')
+axios.get('https://www.youtube.com/feeds/videos.xml?channel_id=TU_ID_DE_CANAL')
     .then(response => {
         parser.parseStringPromise(response.data).then(result => {
             const entries = result.feed.entry;
-            const videos = entries.map(entry => {
+            let videos = '<table><tr>';
+            entries.forEach((entry, index) => {
                 const videoId = entry['yt:videoId'][0];
                 const title = entry.title[0];
                 const thumbnail = `https://img.youtube.com/vi/${videoId}/0.jpg`;
-                return `- [![${title}](${thumbnail})](https://www.youtube.com/watch?v=${videoId})`;
+                videos += `<td align="center"><a href="https://www.youtube.com/watch?v=${videoId}"><img src="${thumbnail}" alt="${title}" style="width:210px;"><br>${title}</a></td>`;
+                if ((index + 1) % 3 === 0) {
+                    videos += '</tr><tr>';
+                }
             });
-            console.log(videos.join('\n'));
+            videos += '</tr></table>';
+            console.log(videos);
         });
     })
     .catch(error => {
