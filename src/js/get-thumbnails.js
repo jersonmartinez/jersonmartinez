3ca166ca0b26ff7e27ccc4839a396f5433a14e26
Jerson Martínez <hotaka01@grupomrw.onmicrosoft.com>
2024-02-09 09:01:42 -0600

    Update thumbnail height in get-thumbnails.js

--- src/js/get-thumbnails.js
+++ src/js/get-thumbnails.js
@@ -13,7 +13,7 @@ axios.get('https://www.youtube.com/feeds/videos.xml?channel_id=UCHQb90WIYhLUObEc
                 const videoId = entry['yt:videoId'][0];
                 const title = entry.title[0];
                 const thumbnail = `https://img.youtube.com/vi/${videoId}/0.jpg`;
-                videos += `<td style="padding: 0px; margin:0px; text-align: center;"><a href="https://www.youtube.com/watch?v=${videoId}" target="_blank"><img src="${thumbnail}" alt="${title}" width="480" height="360"><br>${title}</a></td>`;
+                videos += `<td style="padding: 0px; margin:0px; text-align: center;"><a href="https://www.youtube.com/watch?v=${videoId}" target="_blank"><img src="${thumbnail}" alt="${title}" height="100%"><br>${title}</a></td>`;
                 if ((index + 1) % 3 === 0) {
                     videos += '</tr><tr>';
                 }
