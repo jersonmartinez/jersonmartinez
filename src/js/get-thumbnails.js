d626d2bd538c856903be29648b889991b5e970e1
Jerson Martínez <hotaka01@grupomrw.onmicrosoft.com>
2024-02-09 09:22:20 -0600

    Refactor video thumbnail display layout

--- src/js/get-thumbnails.js
+++ src/js/get-thumbnails.js
@@ -8,18 +8,17 @@ axios.get('https://www.youtube.com/feeds/videos.xml?channel_id=UCHQb90WIYhLUObEc
     .then(response => {
         parser.parseStringPromise(response.data).then(result => {
             const entries = result.feed.entry.slice(0, MAX_RESULTS);
-            let videos = '<div style="display: flex; flex-wrap: wrap;">';
+            let videos = '<table style="width: 100%; overflow-y: hidden; overflow-x: hidden; border: none;"><tr>';
             entries.forEach((entry, index) => {
                 const videoId = entry['yt:videoId'][0];
                 const title = entry.title[0];
                 const thumbnail = `https://img.youtube.com/vi/${videoId}/0.jpg`;
-                videos += `<div width="30%"><a href="https://www.youtube.com/watch?v=${videoId}" target="_blank"><img src="${thumbnail}" alt="${title}" style="max-width: 100%;" ><br>${title}</a></div>`;
-                // Add a line break after every 3 videos
+                videos += `<td style="padding: 0px; margin:0px; text-align: center;"><a href="https://www.youtube.com/watch?v=${videoId}" target="_blank"><img src="${thumbnail}" alt="${title}" width="0%" style="max-width: 100%;" ><br>${title}</a></td>`;
                 if ((index + 1) % 3 === 0) {
-                    videos += '</div><div style="display: flex; flex-wrap: wrap;">';
+                    videos += '</tr><tr>';
                 }
             });
-            videos += '</div>';
+            videos += '</tr></table>';
             console.log(videos);
         });
     })
