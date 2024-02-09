f710e8cc5bc77e89804522a54d9bb3d6316e4260
Jerson Martínez <hotaka01@grupomrw.onmicrosoft.com>
2024-02-09 01:38:15 -0600

    Adding GitHub Actions

--- src/js/get-thumbnails.js
+++ src/js/get-thumbnails.js
@@ -2,17 +2,18 @@ const axios = require('axios');
 const xml2js = require('xml2js');
 
 const parser = new xml2js.Parser();
+const MAX_RESULTS = 6; // Limit to 6 videos
 
 axios.get('https://www.youtube.com/feeds/videos.xml?channel_id=UCHQb90WIYhLUObEc8uVJR6A')
     .then(response => {
         parser.parseStringPromise(response.data).then(result => {
-            const entries = result.feed.entry;
+            const entries = result.feed.entry.slice(0, MAX_RESULTS);
             let videos = '<table><tr>';
             entries.forEach((entry, index) => {
                 const videoId = entry['yt:videoId'][0];
                 const title = entry.title[0];
                 const thumbnail = `https://img.youtube.com/vi/${videoId}/0.jpg`;
-                videos += `<td align="center"><a href="https://www.youtube.com/watch?v=${videoId}"><img src="${thumbnail}" alt="${title}" style="width:210px;"><br>${title}</a></td>`;
+                videos += `<td align="center"><a href="https://www.youtube.com/watch?v=${videoId}" target="_blank"><img src="${thumbnail}" alt="${title}" style="width:210px;"><br>${title}</a></td>`;
                 if ((index + 1) % 3 === 0) {
                     videos += '</tr><tr>';
                 }
